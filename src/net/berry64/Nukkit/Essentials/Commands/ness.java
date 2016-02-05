1bc196b339f2b402c8438b82aa9b502c5cc9a0b0
berry64 <spamnut@yeah.net>
2016-02-05 09:57:31 +0800

    Create ness.java

---
+++ src/net/berry64/Nukkit/Essentials/Commands/ness.java
@@ -0,0 +1,10 @@
+package net.berry64.Nukkit.Essentials.Commands;
+
+import cn.nukkit.command.CommandSender;
+import net.berry64.Nukkit.Essentials.NukkitEssentials;
+
+public class ness {
+	public static void runCommand(CommandSender s, String[] args, NukkitEssentials pl){
+		
+	}
+}
